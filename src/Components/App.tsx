86ae6601bfd7d7eb81456c05eea47b1c19d89133
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-03-27 20:33:01 +0545

    Linking

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "../Screens/Home/Home";
+import Layout from "./Layout/Layout";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
