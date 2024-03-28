b9b07d101b2bdb234d056d832d1db12cda32d418
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-03-28 17:23:51 +0545

    Login screen

--- src/Screens/Login/Login.tsx
+++ src/Screens/Login/Login.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import WomenImg from "../../Assets/women.png";
+import LogoImg from "../../Assets/logo.png";
+import "../../Css/Register/register.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="register">
+      <div className="left">
+        <div className="already">
+          Don't have an account?{" "}
+          <Link className="link" to={"/register"}>
+            Sign up
+          </Link>
+        </div>
+        <button>Login</button>
+        <input placeholder="Password" type="password" id="password" />
+        <label className="registerLabel" htmlFor="password">
+          Password
+        </label>
+        <input placeholder="Email" type="email" id="email" />
+        <label className="registerLabel" htmlFor="email">
+          Email
+        </label>
+        <div className="branding">
+          <img alt="Logo" src={LogoImg} />
+          <div className="logo">Fitness Mania</div>
+        </div>
+      </div>
+      <img alt="Strong Women" src={WomenImg} />
+    </div>
+  );
 };
 
 export default Login;
