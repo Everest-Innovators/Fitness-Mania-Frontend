2a99bb8f8e7fe623f412cd9068363b4b53b95155
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-03-28 17:15:53 +0545

    Register Screen

--- src/Screens/Register/Register.tsx
+++ src/Screens/Register/Register.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import WomenImg from "../../Assets/women.png";
 import LogoImg from "../../Assets/logo.png";
+import "../../Css/Register/register.css";
 
 const Register = () => {
   return (
     <div className="register">
       <div className="left">
+        <button>Register</button>
+        <input placeholder="Password" type="password" id="password" />
+        <label className="registerLabel" htmlFor="password">
+          Password
+        </label>
+        <input placeholder="Email" type="email" id="email" />
+        <label className="registerLabel" htmlFor="email">
+          Email
+        </label>
+        <input placeholder="Display Name" type="text" id="displayName" />
+        <label className="registerLabel" htmlFor="displayName">
+          Display Name
+        </label>
+        <input placeholder="Username" type="text" id="username" />
+        <label className="registerLabel" htmlFor="username">
+          Username
+        </label>
         <div className="branding">
           <img alt="Logo" src={LogoImg} />
-          <div className="logo">Something</div>
+          <div className="logo">Fitness Mania</div>
         </div>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-        <label htmlFor="displayName">Display Name</label>
-        <input type="text" id="displayName" />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
       </div>
       <img alt="Strong Women" src={WomenImg} />
     </div>
