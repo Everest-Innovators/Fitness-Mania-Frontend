917b4d51ef22599c1e202603a6464146a7d3c6ea
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-04-03 19:09:12 +0545

    Create Structure

--- src/Screens/Create/Create.tsx
+++ src/Screens/Create/Create.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import imagePng from "../../Assets/image.png";
 
 const Create = () => {
   return (
-    <div>Create</div>
-  )
-}
+    <div className="create">
+      <input type="text" />
+      <textarea></textarea>
+      <div>
+        <img src={imagePng} alt="Attach" />
+        <button>Post</button>
+      </div>
+    </div>
+  );
+};
 
-export default Create
+export default Create;
