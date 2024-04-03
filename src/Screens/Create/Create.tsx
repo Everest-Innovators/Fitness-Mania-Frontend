c8475f70a83b6ae9c042ef79a7dc7eaff9f31d0d
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-04-03 19:29:10 +0545

    Finish Create Post

--- src/Screens/Create/Create.tsx
+++ src/Screens/Create/Create.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import imagePng from "../../Assets/image.png";
+import "../../Css/Create/Create.css";
 
 const Create = () => {
   return (
     <div className="create">
-      <input type="text" />
-      <textarea></textarea>
-      <div>
-        <img src={imagePng} alt="Attach" />
-        <button>Post</button>
+      <div className="container">
+        <div className="title">Create a post</div>
+        <input placeholder="Title" type="text" />
+        <textarea placeholder="Description" />
+        <div>
+          <img src={imagePng} alt="Attach" />
+          <button>Post</button>
+        </div>
       </div>
     </div>
   );
