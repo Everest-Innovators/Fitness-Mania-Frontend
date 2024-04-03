c5e1cba4e8a65cfe6e55348cb0238b48d826fab0
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-04-03 22:07:45 +0545

    Login

--- src/Screens/Login/Login.tsx
+++ src/Screens/Login/Login.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WomenImg from "../../Assets/women.png";
 import LogoImg from "../../Assets/logo.png";
 import "../../Css/Register/Register.css";
-import { Link } from "react-router-dom";
+import { Link, NavigateFunction, useNavigate } from "react-router-dom";
+import ValidUser from "../../Utilities/ValidUser";
+import { api_url } from "../../Utilities/Constants";
 
 const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const navigate: NavigateFunction = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      let status = await ValidUser();
+      if (status) navigate("/");
+    })();
+  }, [navigate]);
+
+  const onSubmit = async () => {
+    const res = await fetch(`${api_url}/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application.json",
+      },
+    });
+    let resData = await res.json();
+    if (res.status === 200) {
+      window.localStorage.setItem("id", resData.id);
+      window.localStorage.setItem("password", password);
+      navigate("/");
+    }
+  };
+
   return (
     <div className="register">
       <div className="left">
@@ -14,12 +47,12 @@ const Login = () => {
             Sign up
           </Link>
         </div>
-        <button>Login</button>
-        <input placeholder="Password" type="password" id="password" />
+        <button onClick={onSubmit}>Login</button>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" id="password" />
         <label className="registerLabel" htmlFor="password">
           Password
         </label>
-        <input placeholder="Email" type="email" id="email" />
+        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" id="email" />
         <label className="registerLabel" htmlFor="email">
           Email
         </label>
