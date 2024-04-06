d24654f3e52cfdb62148cc2adba86dcc2d1a6fdc
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2024-04-06 13:32:47 +0545

    Render posts

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "../../Components/Post/Post";
 import Avatar from "../../Assets/avatar.png";
 import "../../Css/Home/Home.css";
 import { api_url } from "../../Utilities/Constants";
 
 const Home = () => {
+  const [posts, setPosts] = useState<Array<JSX.Element>>([]);
   useEffect(() => {
     (async () => {
       const res = await fetch(`${api_url}/latest`, {
@@ -14,46 +15,28 @@ const Home = () => {
         },
       });
       let resData = await res.json();
+      let tempPost = [];
+      for (let i = 0; i < resData.length; i++) {
+        tempPost.push(
+          <Post
+            avatar={Avatar}
+            username="new_"
+            timestamp={new Date(resData[i][5]).getTime()}
+            body={resData[i][4]}
+            comments={resData[i][8] ? resData[i][8].length : 0}
+            dislike={resData[i][7] ? resData[i][7].length : 0}
+            like={resData[i][6] ? resData[i][6].length : 0}
+            postId={resData[i][0]}
+            title={resData[i][2]}
+          />
+        );
+      }
+      setPosts(tempPost);
     })();
   });
   return (
     <div className="home">
-      <div className="posts">
-        <Post
-          avatar={Avatar}
-          username="new__"
-          timestamp={Date.now() - 100000}
-          title="[AskJS] Coding a webpage button that redirects to a random URL (read body text)"
-          body={`Hello. I'm working on a Neocities website and I want to create a button that redirects the user to a random web URL when clicked (purely for fun).
-
-But here's the problem: even after following several guides I discovered on places like Quora and Stack Overflow, my code still doesn't seem to work at all — I've followed several different scripts I found online to no avail, and my button just redirects to a text page of the Javascript itself (image of both code strings here, issue lies within line 97).
-
-I also checked through the rest of my website's code and couldn't find any sort of conflicting elements, so the issue probably lies within the way in which I formatted the "MysteryLink" JavaScript (or the HREF string itself).
-
-I'm wondering if anyone here can guide me towards a working method of fixing this, or provide an alternative method for achieving the effect entirely. Any an all suggestions will be appreciated, thank you all in advance for your time.`}
-          postId={1232}
-          comments={12}
-          like={53}
-          dislike={7}
-        />
-        <Post
-          avatar={Avatar}
-          username="new__"
-          timestamp={Date.now() - 100000}
-          title="[AskJS] Coding a webpage button that redirects to a random URL (read body text)"
-          body={`Hello. I'm working on a Neocities website and I want to create a button that redirects the user to a random web URL when clicked (purely for fun).
-
-But here's the problem: even after following several guides I discovered on places like Quora and Stack Overflow, my code still doesn't seem to work at all — I've followed several different scripts I found online to no avail, and my button just redirects to a text page of the Javascript itself (image of both code strings here, issue lies within line 97).
-
-I also checked through the rest of my website's code and couldn't find any sort of conflicting elements, so the issue probably lies within the way in which I formatted the "MysteryLink" JavaScript (or the HREF string itself).
-
-I'm wondering if anyone here can guide me towards a working method of fixing this, or provide an alternative method for achieving the effect entirely. Any an all suggestions will be appreciated, thank you all in advance for your time.`}
-          postId={1232}
-          comments={12}
-          like={53}
-          dislike={7}
-        />
-      </div>
+      <div className="posts">{posts}</div>
     </div>
   );
 };
